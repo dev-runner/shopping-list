a538ef15db33828dba4530b8fa6bd0593390512f
przemek <przemek@devrunner.pl>
2016-07-02 16:58:20 +0200

    added a simple ShoppingListController

--- app/app.js
+++ app/app.js
@@ -3,14 +3,26 @@
 var dependencies = ['ngRoute', 'ngAnimate', 'ngTouch', 'ui.bootstrap'];
 var app = angular.module('myApp', dependencies);
 
+
+// app configuration
 app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
 	
-	$locationProvider.hashPrefix('!');
-
-	// default route
-	$routeProvider.otherwise({
-		redirectTo: '/'
-	});
+    $locationProvider.hashPrefix('!');
+    
+    $routeProvider.otherwise({
+        redirectTo: '/'
+    });
 
 }]);
 
+
+// shop list controller
+app.controller('ShoppingListController',['$scope', function($scope){
+    
+    $scope.shopList = ['Milk', 'Butter', 'Bread', 'Cheese'];
+    
+    $scope.addItem = function(item){
+        $scope.shopList.push(item);
+    }
+    
+}]);

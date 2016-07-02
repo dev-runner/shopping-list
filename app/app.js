0d3f5a93214da2a7fc6c12551f2ff8d4f2247752
przemek <przemek@devrunner.pl>
2016-07-02 22:05:27 +0200

    nice styling added

--- app/app.js
+++ app/app.js
@@ -19,10 +19,14 @@ app.config(['$locationProvider', '$routeProvider', function($locationProvider, $
 // shop list controller
 app.controller('ShoppingListController',['$scope', function($scope){
     
-    $scope.shopList = ['Milk', 'Butter', 'Bread', 'Cheese'];
+    $scope.title = 'My shopping list';
+    
+    // todo: get the initial list from json file
+    $scope.shopList = [];
     
     $scope.errorText = '';
     
+    // add new item to the shopping list
     $scope.addItem = function(){
         if(!$scope.newItem) return;
         if($scope.shopList.indexOf($scope.newItem) == -1){
@@ -34,6 +38,7 @@ app.controller('ShoppingListController',['$scope', function($scope){
         
     }
     
+    // removes the item from the shopping list
     $scope.removeItem = function(index){
         $scope.errorText = '';
         $scope.shopList.splice(index,1);

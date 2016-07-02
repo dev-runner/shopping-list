103d7df29700b46fe082ee0aff60d52e04ac6536
przemek <przemek@devrunner.pl>
2016-07-02 17:43:55 +0200

    error handling added

--- app/app.js
+++ app/app.js
@@ -21,11 +21,21 @@ app.controller('ShoppingListController',['$scope', function($scope){
     
     $scope.shopList = ['Milk', 'Butter', 'Bread', 'Cheese'];
     
+    $scope.errorText = '';
+    
     $scope.addItem = function(){
-        $scope.shopList.push($scope.newItem);
+        if(!$scope.newItem) return;
+        if($scope.shopList.indexOf($scope.newItem) == -1){
+            $scope.shopList.push($scope.newItem);
+            $scope.newItem = '';
+        } else {
+            $scope.errorText = 'This item is already in the shopping list.';
+        }
+        
     }
     
     $scope.removeItem = function(index){
+        $scope.errorText = '';
         $scope.shopList.splice(index,1);
     }
     
